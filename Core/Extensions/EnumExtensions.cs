5e427243854ed6ab472ad0ad11e4b0b49d98448b
Tantax <tantax-tang@foxmail.com>
2016-10-27 15:25:26 +0800

    添加单元测试和Distinct去重扩展
    1、单元测试研究中
    2、是对IEnumerable中的Distinct方法扩展

---
+++ Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public class EnumExtensions
+    {
+        /// <summary>
+        /// 通过把每个函数添加到hash表中来过滤重复的数据
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
